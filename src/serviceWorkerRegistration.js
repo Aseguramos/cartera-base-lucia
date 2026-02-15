08ac1e999d0e951c2fd388e2307e51cd1284e53b
Aseguramos <aseguramosjl@hotmail.com>
2026-02-14 20:52:25 -0500

    fix sw real

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -4,8 +4,8 @@ export function register() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
 
-      navigator.serviceWorker
-        .register('/service-worker.js')
+     navigator.serviceWorker
+  .register('/sw.js')
         .then((registration) => {
 
           console.log('SW registrado');
