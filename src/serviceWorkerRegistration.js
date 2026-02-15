4b95427d1f7d0a621eb7be32497858fadc7818b3
Aseguramos <aseguramosjl@hotmail.com>
2026-02-14 20:27:15 -0500

    activar cache offline real

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,15 +1,23 @@
 // serviceWorkerRegistration.js
+
 export function register() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
+
       navigator.serviceWorker
         .register('/service-worker.js')
         .then((registration) => {
-          console.log('SW registrado:', registration);
+
+          console.log('SW registrado');
+
+          // 🔥 FORZAR CACHE OFFLINE
+          registration.update();
+
         })
         .catch((error) => {
           console.log('Error SW:', error);
         });
+
     });
   }
 }
