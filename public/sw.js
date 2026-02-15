194e5feaacece59287b121d437f0515e40cc13b6
Aseguramos <aseguramosjl@hotmail.com>
2026-02-15 11:22:46 -0500

    fix offline react real

--- public/sw.js
+++ public/sw.js
@@ -1,28 +1,25 @@
-const CACHE_NAME = "cartera-offline-v1";
+const CACHE_NAME = "cartera-react-cache-v3";
 
-const urlsToCache = [
-  "/",
-  "/index.html"
-];
-
-// INSTALACIÓN
 self.addEventListener("install", (event) => {
-  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
+      return cache.addAll([
+        "/",
+        "/index.html"
+      ]);
     })
   );
+  self.skipWaiting();
 });
 
-// ACTIVAR
 self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
 });
 
-// OFFLINE REAL
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match("/index.html"))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
